b96069df17b89ab8e686eb6d99e3984470c5d311
IAMLLT796 <lt.liu796@gmail.com>
2024-01-24 21:51:52 +0800

    客户端菜单功能的开发

---
+++ client.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ServerIp   string
+	ServerPort int
+	Name       string
+	conn       net.Conn
+	flag       int // 当前 client 的模式
+}
+
+var serverIp string
+var serverPort int
+
+// ./client -ip 127.0.0.1 -port
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址(默认是127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口(默认是8888)")
+}
+
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>请输入合法范围内的数字<<<<")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+		}
+
+		// 根据不同的模式处理不同的业务
+		switch client.flag {
+		case 1:
+			// 公聊模式
+			fmt.Println("公聊模式选择...")
+			break
+		case 2:
+			// 私聊模式
+			fmt.Println("私聊模式选择...")
+			break
+		case 3:
+			// 更改用户名
+			fmt.Println("更改用户名选择...")
+			break
+		}
+	}
+}
+
+func NewClient(serverIp string, serverPort int) *Client {
+	client := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+		flag:       999,
+	}
+	// 连接 server
+	conn, err := net.Dial("tcp", fmt.Sprintf(serverIp, serverPort))
+	if err != nil {
+		fmt.Println("ner Dial error: ", err)
+		return nil
+	}
+	client.conn = conn
+	// 返回对象
+	return client
+}
+
+func main() {
+	// 命令行解析
+	flag.Parse()
+
+	client := NewClient("127.0.0.1", 8888)
+	if client == nil {
+		fmt.Println(">>>>>>>>> 链接服务器失败...")
+		return
+	}
+	fmt.Println(">>>>>>>>>>链接服务器成功...")
+
+	// 启动客户端的业务
+	client.Run()
+}
